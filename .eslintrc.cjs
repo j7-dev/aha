daf6bd13d073d99e54e625c4981150e5bd18ad65
J7 <j7.dev.gg@outlook.com>
2024-03-31 16:24:30 +0800

    feat: add airbnb preset & eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,44 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "airbnb",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
   },
-}
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+      typescript: {
+        project: "./tsconfig.json",
+      },
+      alias: {
+        map: [["@", path.resolve(__dirname, "./src")]],
+        extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
+      },
+    },
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import alias from "@rollup/plugin-alias";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import alias from '@rollup/plugin-alias';
+import path from 'path';
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
-  plugins: [alias(), react(), tsconfigPaths()],
+  plugins: [
+    alias(),
+    react(),
+    tsconfigPaths(),
+    babel({
+      babelConfig: {
+        presets: ['airbnb'],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
